5daf4d7523e9576703ba3b7211115b114b355915
HettOne <153020032+HettOne@users.noreply.github.com>
2025-08-18 15:57:57 -0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -3,5 +3,5 @@ module.exports = {
     ticket_channel: "1346315760867348521",
     ticket_category: "1247404144004956201",
     ticket_logs: "1312829897521107014",
-    support_team: "1066309164957171720"
+    support_team: "1066309164957171720",
 }
