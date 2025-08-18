bf96f5f92717198bf0503bb417e5af27b57a2294
HettOne <153020032+HettOne@users.noreply.github.com>
2025-08-18 14:47:03 -0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     token: "MTMwMjU5ODYwMjcwMjcyMTAzNg.GZVl4z.LeY51Hp7rn08clJxZsKyj3kU94901orZwHwFy0",
-    ticket_channel: "1353571601874030675",
-    ticket_category: "1353571580415967262",
-    ticket_logs: "1353571777623625879",
+    ticket_channel: "1346315760867348521",
+    ticket_category: "1247404144004956201",
+    ticket_logs: "1312829897521107014",
     support_team: "1066309164957171720"
 }
